6b5aefe17c9781299bed0af8ec662e44c0cbd539
Bryn Ryans <bryn.ryans@looker.com>
2019-09-20 14:34:49 -0700

    sidebar

--- sidebar_prototype.js
+++ sidebar_prototype.js
@@ -1,30 +1,63 @@
+
+// The configuration object needs to be created using an API.
+// Chatty should be used to send the message.
+// An API should probably be built on top of chatty that configures
+// and sends the message.
+
 window.parent.postMessage({
 	payload: {
 		event: 'extensions_dsl',
 		component: {
-			type: 'flow_layout',
+			type: 'row_layout',
 			children: [
 				{
-					type: 'dashboard',
-					dashboardId: 'block_bing_ads::bing_ads_clicks',
-					height: 300,
-					width: 400
+					type: 'sidebar',
+					initialTargetId: 'bing_ads_clicks_dashboard',
+					width: 250,
+					heading: {
+						label: 'My Sidebar',
+						icon: 'Flag'
+					},
+					items: [
+						{
+							label: 'Dashboard (TODO)',
+							icon: 'Dashboard',
+							targetId: 'bing_ads_clicks_dashboard',
+						},
+						{
+							label: 'Example.com',
+							icon: 'External',
+							targetId: 'example_iframe',
+						},
+						{
+							label: "Will's Special!",
+							icon: 'ApplicationSelect',
+							targetId: 'wils_special_iframe',
+						},
+					]
 				},
 				{
-					type: 'iframe',
-					srcUrl: 'https://example.com',
-					height: 300,
-					width: 400
+					type: 'card_layout',
+					targets: [
+						{
+							id: 'bing_ads_clicks_dashboard',
+							type: 'dashboard',
+							dashboardId: 'block_bing_ads::bing_ads_clicks',
+						},
+						{
+							id: 'example_iframe',
+							type: 'iframe',
+							srcUrl: 'https://example.com',
+						},
+						{
+							id: 'wils_special_iframe',
+							type: 'iframe',
+							jsName: 'hello.js',
+						}
+					]
 				},
-				{
-					type: 'iframe',
-					jsName: 'hello.js',
-					height: 300,
-					width: 400
-				}
 			]
 		}
 	},
 	source: 'extension_framework_client'
 }, window.parent.location.origin)
-
